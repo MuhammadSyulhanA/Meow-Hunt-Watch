93cc801a38e3c2c4fc10f7baaed8b61dab4e24d5
MuhammadSyulhanA <janmuno77@gmail.com>
2022-07-15 11:02:06 +0800

    Initial Commit

---
+++ Meow Hunt Watch WatchKit Extension/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Meow Hunt Watch WatchKit Extension
+//
+//  Created by M. Syulhan Al Ghofany on 15/07/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Meow Hunt Watch WatchKit Extension/Meow_Hunt_WatchApp.swift
@@ -0,0 +1,19 @@
+//
+//  Meow_Hunt_WatchApp.swift
+//  Meow Hunt Watch WatchKit Extension
+//
+//  Created by M. Syulhan Al Ghofany on 15/07/22.
+//
+
+import SwiftUI
+
+@main
+struct Meow_Hunt_WatchApp: App {
+    var body: some Scene {
+        WindowGroup {
+            NavigationView {
+                ContentView()
+            }
+        }
+    }
+}
